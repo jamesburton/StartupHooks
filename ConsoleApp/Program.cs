86e410a029739cfb009fbfe868a200087851cf33
jamesburton <james@code-consultants.co.uk>
2023-02-02 12:06:41 +0000

    Added delay and await key-press to example console app

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Thread.Sleep(750);
+
+Console.WriteLine("- Press a key to close");
+
+Console.ReadKey();
--- InvertedConsole/StartupHook.cs
+++ InvertedConsole/StartupHook.cs
@@ -1,5 +1,22 @@
+using System.Diagnostics;
+
 class StartupHook
 {
-    public static void Initialize() =>
+    // public static void Initialize() =>
+    //     Console.SetOut(new InvertedTextWriter(Console.Out));
+
+    private static Stopwatch _stopwatch = new Stopwatch();
+    public static void Initialize() 
+    {
+        var oldOut = Console.Out;
+        Console.WriteLine($"=~= Starting inversion process @ {DateTime.UtcNow:O}");
         Console.SetOut(new InvertedTextWriter(Console.Out));
+        _stopwatch.Start();
+        AppDomain.CurrentDomain.ProcessExit += (s, e) => {
+            _stopwatch.Stop();
+            Console.SetOut(oldOut);
+            Console.WriteLine($"=~= End of inversion process @ {DateTime.UtcNow:O}");
+            Console.WriteLine($"- Interval: {_stopwatch.Elapsed}");
+        };
+    }
 }
